f3dfd64d48c7d38c650a0f8e4b84ea8b1fce8fd1
Jintian Wang <wangjintianlovemc@163.com>
2026-07-23 13:21:39 +0800

    test: 用 findBy 等待城市选择弹层渲染

--- __tests__/new-memory-city-selector.test.tsx
+++ __tests__/new-memory-city-selector.test.tsx
@@ -21,7 +21,7 @@ describe("new memory city selector", () => {
     expect(screen.getByLabelText("已选城市 北京")).toBeTruthy();
 
     fireEvent.press(screen.getByLabelText("选择地点"));
-    fireEvent.changeText(screen.getByLabelText("搜索城市"), "台北");
+    fireEvent.changeText(await screen.findByLabelText("搜索城市"), "台北");
 
     await waitFor(() => expect(screen.getByText("台北 · 台湾省")).toBeTruthy());
     expect(screen.queryByText("北京 · 北京市")).toBeNull();
@@ -33,9 +33,8 @@ describe("new memory city selector", () => {
     expect(screen.queryByText("生成旅行册草稿")).toBeNull();
 
     fireEvent.press(screen.getByLabelText("选择地点"));
-    fireEvent.changeText(screen.getByLabelText("搜索城市"), "杭州");
-    await waitFor(() => expect(screen.getByText("杭州 · 浙江省")).toBeTruthy());
-    fireEvent.press(screen.getByText("杭州 · 浙江省"));
+    fireEvent.changeText(await screen.findByLabelText("搜索城市"), "杭州");
+    fireEvent.press(await screen.findByText("杭州 · 浙江省"));
 
     await waitFor(() => expect(screen.getByLabelText("已选城市 杭州")).toBeTruthy());
   });
